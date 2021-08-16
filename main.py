c82704f33c51fc43e6e96783c091f2559df99030
jer123se12 <64419693+jer123se12@users.noreply.github.com>
2021-08-16 17:20:11 +0800

    MUCH BETTER.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import requests
-def download(url):
+import threading
+import time
+from urllib.request import urlretrieve
+import concurrent
+def download(url,t):
     x=requests.get(url)
     data=x.json()
     data2=[]
@@ -7,21 +11,43 @@ def download(url):
         data2.append(str(i["SetId"]))
     data=data2
     path="./downloads/"
+    threads=[]
     for i in data:
-        print("downloading"+i+"now")
-        url="https://api.chimu.moe/v1/download/"+i+"?n=0"
-        r = requests.get(url, allow_redirects=True)
-        if r.content!="":
-            with open(path+i+'.osz', 'wb') as a:
-                a.write(r.content)
+        
+        starttime=time.time()
+        threads.append(threading.Thread(target=dl2, args=(url,path,i)))
+    active=[]
+    for i in range(len(threads)):
+        threads[i].start()
+        active.append(i)
+        if len(active)>t:
+            for i in active:
+                threads[i].join()
+            active=[]
+    for i in active:
+        threads[i].join()
+def dl2(url,path,i):
+    starttime=time.time()
+    print("downloading "+i+"st file now")
+    r = requests.get(url, allow_redirects=True)
+    if len(r.content)>1:
+        print("saving at: "+str(time.time()-starttime))
+        with open(path+i+'.osz', 'wb') as a:
+            a.write(r.content)
+    print("done, time taken: "+str(time.time()-starttime))
+    
 p=str(int(input("amt of files: "))-1)
-o=input("offset")
+o=input("offset: ")
 o= o if len(o)>0 else 0
 k=input("amt of keys: ")
-if int(p)>100:
-    for i in range(int(p)%100):
+t=int(input("threads: "))
+totalt=time.time()
+if int(p)>50:
+    for i in range(int(p)%50):
         if i>=0:
             o=0
-        download(f"https://api.chimu.moe/v1/search?query=&mode=3&min_cs={k}&max_cs={k}&amount=100&offset={str(i+int(o))}00&status=1")
-url=f"https://api.chimu.moe/v1/search?query=&mode=3&min_cs={k}&max_cs={k}&amount={str((int(p)%100))}&offset={o}"
-download(url)
+        download(f"https://api.chimu.moe/v1/search?query=&mode=3&min_cs={k}&max_cs={k}&amount=50&offset={str((i*50)+int(o))}&status=1",t)
+url=f"https://api.chimu.moe/v1/search?query=&mode=3&min_cs={k}&max_cs={k}&amount={str((int(p)%50))}&offset={o}"
+print(url)
+download(url,t)
+print(f"DONE!!!!!\nTime take: {str(time.time()-totalt)}")
