e898e6c88704d68f90bb0599d16fa421f78a4d92
jer123se12 <jerickseng06@gmail.com>
2021-08-16 22:46:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ def download(url,t):
     data2=[]
     for i in data["data"]:
         data2.append(str(i["SetId"]))
-    data=data2
+    return data2
+def d(data):
     path="./downloads/"
     threads=[]
     for i in data:
-        
         starttime=time.time()
         url="https://api.chimu.moe/v1/download/"+i+"?n=0"
         threads.append(threading.Thread(target=dl2, args=(url,path,i)))
@@ -29,13 +29,13 @@ def download(url,t):
         threads[i].join()
 def dl2(url,path,i):
     starttime=time.time()
-    print("downloading "+i+"st file now")
+    print("downloading file with setid: "+i+" now")
     r = requests.get(url, allow_redirects=True)
     if len(r.content)>1:
-        print("saving at: "+str(time.time()-starttime))
+        print("saving "+str(i)+" at: "+str(time.time()-starttime))
         with open(path+i+'.osz', 'wb') as a:
             a.write(r.content)
-    print("done, time taken: "+str(time.time()-starttime))
+    print(str(i)+" done, time taken: "+str(time.time()-starttime))
     
 p=str(int(input("amt of files: "))-1)
 o=input("offset: ")
@@ -43,12 +43,16 @@ o= o if len(o)>0 else 0
 k=input("amt of keys: ")
 t=int(input("threads: "))
 totalt=time.time()
-if int(p)>50:
-    for i in range(int(p)%50):
+data=[]
+if int(p)>100:
+    for i in range(int(p)//100):
+        print(str(i)+"loaded out of "+str(int(p)//100))
         if i>=0:
             o=0
-        download(f"https://api.chimu.moe/v1/search?query=&mode=3&min_cs={k}&max_cs={k}&amount=50&offset={str((i*50)+int(o))}&status=1",t)
-url=f"https://api.chimu.moe/v1/search?query=&mode=3&min_cs={k}&max_cs={k}&amount={str((int(p)%50))}&offset={o}"
+        data+=download(f"https://api.chimu.moe/v1/search?query=&mode=3&min_cs={k}&max_cs={k}&amount=100&offset={str((i*100)+int(o))}&status=1",t)
+
+url=f"https://api.chimu.moe/v1/search?query=&mode=3&min_cs={k}&max_cs={k}&amount={str((int(p)%100))}&offset={o}"
 print(url)
-download(url,t)
+data+=download(url,t)
+d(data)
 print(f"DONE!!!!!\nTime take: {str(time.time()-totalt)}")
